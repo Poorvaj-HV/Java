b367ab12e172b4162506ec3144934b4bacf8ecf7
Poorvaj H V <poorvajvishwanath19@gmail.com>
2025-05-29 00:46:54 +0530

    Time: 6 ms (7.08%), Space: 57.4 MB (86.2%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -29,9 +29,4 @@ class Solution {
 
         return maxC;
     }
-
-    public static void main(String[] args) {
-        int height[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
-        System.out.println(maxArea(height));
-    }
 }
