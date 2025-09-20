61d01e135643aa1e9c6cb57d3da94a6a904f9e76
Poorvaj H V <poorvajvishwanath19@gmail.com>
2025-09-20 23:32:23 +0530

    Time: 0 ms (100%), Space: 41 MB (78.72%) - LeetHub

--- 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.java
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.java
@@ -10,22 +10,37 @@
  */
 class Solution {
     public int getDecimalValue(ListNode head) {
-        StringBuilder str = new StringBuilder("");
+        // StringBuilder str = new StringBuilder("");
 
+        // ListNode temp = head;
+        // while(temp != null) {
+        //     str.append(temp.val);
+        //     temp = temp.next;
+        // }
+        // String st = str.toString();
+
+        // int pos = 1;
+        // int result = 0;
+        // for(int i=st.length()-1; i>=0; i--) {
+        //     result += Integer.parseInt(String.valueOf(st.charAt(i))) * pos;
+        //     pos *= 2;
+        // }
+
+        int size = 0;
         ListNode temp = head;
         while(temp != null) {
-            str.append(temp.val);
+            temp = temp.next;
+            size++;
+        }
+
+        temp = head;
+        int ans = 0;
+        while(temp != null) {
+            size--;
+            ans += (temp.val * Math.pow(2, size));
             temp = temp.next;
         }
-        String st = str.toString();
 
-        int pos = 1;
-        int result = 0;
-        for(int i=st.length()-1; i>=0; i--) {
-            result += Integer.parseInt(String.valueOf(st.charAt(i))) * pos;
-            pos *= 2;
-        }
-
-        return result;
+        return ans;
     }
 }
