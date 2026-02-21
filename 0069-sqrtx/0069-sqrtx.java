f8db5615884401503d40c4752989a3636725e34e
Poorvaj H V <poorvajvishwanath19@gmail.com>
2026-02-21 12:44:54 +0530

    Time: 1 ms (96.93%), Space: 42.7 MB (41.36%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,29 +1,29 @@
 class Solution {
     public int mySqrt(int x) {
-        // double val = Math.pow(x, 0.5);
-        // return (int)Math.floor(val);
+        double val = Math.pow(x, 0.5);
+        return (int)Math.floor(val);
 
-        if(x < 2) {
-            return x;
-        }
+        // if(x < 2) {
+        //     return x;
+        // }
 
-        int left = 1, right = x / 2;
-        int result = 0;
+        // int left = 1, right = x / 2;
+        // int result = 0;
 
-        while(left <= right) {
-            int mid = left + (right-left) / 2;
-            long square = (long) mid * mid;
+        // while(left <= right) {
+        //     int mid = left + (right-left) / 2;
+        //     long square = (long) mid * mid;
 
-            if(square == x) {
-                return mid;
-            } else if(square < x) {
-                result = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
+        //     if(square == x) {
+        //         return mid;
+        //     } else if(square < x) {
+        //         result = mid;
+        //         left = mid + 1;
+        //     } else {
+        //         right = mid - 1;
+        //     }
+        // }
 
-        return result;
+        // return result;
     }
 }
