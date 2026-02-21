2dd301eae58326fff9982d5fe29393fbf2e1518c
Poorvaj H V <poorvajvishwanath19@gmail.com>
2026-02-21 15:40:00 +0530

    Time: 0 ms (100%), Space: 48.4 MB (21.73%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,19 +1,5 @@
 class Solution {
     public static double myPow(double x, long n) {
-        // long N = n;
-        // if(N == 0) {
-        //     return 1;
-        // }
-
-        // if(N < 0) {
-        //     return 1 / myPow(x, -N);
-        // }
-        // double half = myPow(x, (int)(N / 2));
-        // if(N % 2 == 0) {
-        //     return half * half;
-        // } else {
-        //     return half * half * x;
-        // }
         if(n == 0) {
             return 1;
         }
