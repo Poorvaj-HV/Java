3e0a003aa23e41567bfca475b54163d020ddf682
Poorvaj H V <poorvajvishwanath19@gmail.com>
2026-02-21 15:39:40 +0530

    Time: 0 ms (100%), Space: 48 MB (64.63%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,22 +1,32 @@
 class Solution {
     public static double myPow(double x, long n) {
-        long N = n;
-        if(N == 0) {
+        // long N = n;
+        // if(N == 0) {
+        //     return 1;
+        // }
+
+        // if(N < 0) {
+        //     return 1 / myPow(x, -N);
+        // }
+        // double half = myPow(x, (int)(N / 2));
+        // if(N % 2 == 0) {
+        //     return half * half;
+        // } else {
+        //     return half * half * x;
+        // }
+        if(n == 0) {
             return 1;
         }
 
-        if(N < 0) {
-            return 1 / myPow(x, -N);
-        }
-        double half = myPow(x, (int)(N / 2));
-        if(N % 2 == 0) {
+        if(n < 0) {
+            return 1 / myPow(x, -n);
+        } 
+        
+        double half = myPow(x, n/2);
+        if(n % 2 == 0) {
             return half * half;
         } else {
             return half * half * x;
         }
-    }
-
-    public static void main(String[] args) {
-        System.out.println(myPow(2, 10));
     }
 }
