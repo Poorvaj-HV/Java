4b06071eaebd6bff42224690ce7913ef71a39a75
Poorvaj H V <poorvajvishwanath19@gmail.com>
2026-03-08 10:41:48 +0530

    Time: 0 ms (100%), Space: 42.9 MB (92.57%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,9 +1,9 @@
 class Solution {
     public static int lengthOfLastWord(String s) {
         int n = s.length();
-        if(n == 1 && (s.charAt(0) != ' ')) {
-            return 1;
-        }
+        // if(n == 1 && (s.charAt(0) != ' ')) {
+        //     return 1;
+        // }
 
         int i = n - 1;
         while(s.charAt(i) == ' ') {
@@ -17,20 +17,5 @@ class Solution {
         }
 
         return count;
-        // int right = 0, left = 0;
-        // boolean occurance = true;
-        // for(int i=n-1; i>=0; i--) {
-        //     if(occurance && (s.charAt(i) != ' ')) {
-        //         occurance = false;
-        //         right = i;
-        //     }
-        //     if(i > 0 && (s.charAt(i) != ' ') && (s.charAt(i-1) == ' ')) {
-        //         left = i;
-        //         break;
-        //     }
-        // }
-        // int countLength = s.substring(left, right).length();
-
-        // return countLength + 1;
     }
 }
