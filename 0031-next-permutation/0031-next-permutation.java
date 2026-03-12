958ba05ccb50e297c27c6479870d5f58ad675957
Poorvaj H V <poorvajvishwanath19@gmail.com>
2026-03-12 16:04:03 +0530

    Time: 0 ms (100%), Space: 44.6 MB (66.48%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,45 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int pivot = -1;
+
+        for(int i=n-2; i>=0; i--) {
+            if(nums[i] < nums[i+1]) {
+                pivot = i;
+                break;
+            }
+        }
+
+        if(pivot == -1) {
+            reverseArr(nums, 0, n-1);
+            return;
+        }
+
+        int diff = Integer.MAX_VALUE;
+        int idx = -1;
+        for(int i=n-1; i>pivot; i--) {
+            if(nums[pivot] < nums[i] && Math.abs(nums[pivot] - nums[i]) < diff) {
+                diff = Math.abs(nums[pivot] - nums[i]);
+                idx = i;
+            }
+        }
+
+        int temp = nums[pivot];
+        nums[pivot] = nums[idx];
+        nums[idx] = temp;
+
+        reverseArr(nums, pivot+1, n-1);
+    }
+
+    public int[] reverseArr(int[] nums, int si, int ei) {
+        while(si < ei) {
+            int temp = nums[si];
+            nums[si] = nums[ei];
+            nums[ei] = temp;
+            si++;
+            ei--;
+        }
+
+        return nums;
+    }
+}
