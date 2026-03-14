92c3401dc0f2410918302c23df7dbc09e3afd0df
Poorvaj H V <poorvajvishwanath19@gmail.com>
2026-03-14 10:48:56 +0530

    Time: 0 ms (100%), Space: 44.7 MB (36.54%) - LeetHub

---
+++ 3861-minimum-capacity-box/3861-minimum-capacity-box.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumIndex(int[] capacity, int itemSize) {
+        int ans = -1;
+        int diff = Integer.MAX_VALUE;
+
+        for(int i=0; i<capacity.length; i++) {
+            if(capacity[i] == itemSize) {
+                return i;
+            } else if(capacity[i] > itemSize) {
+                if(capacity[i] - itemSize < diff) {
+                    diff = capacity[i] - itemSize;
+                    ans = i;
+                }
+            }
+        }
+
+        return (ans == Integer.MAX_VALUE) ? -1 : ans;
+    }
+}
