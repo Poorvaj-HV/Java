57fddab375952f31c9cf682afc03992f41f39b28
Poorvaj H V <poorvajvishwanath19@gmail.com>
2026-03-21 09:06:45 +0530

    Time: 1 ms (99.9%), Space: 46.2 MB (93.88%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,62 +10,28 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode temp = new ListNode(0);
-        ListNode tail = temp;
+        ListNode ln = new ListNode(0);
+        ListNode tail = ln;
         int carry = 0;
 
         while(l1 != null || l2 != null || carry != 0) {
-            int digit1 = (l1 != null) ? l1.val : 0;
-            int digit2 = (l2 != null) ? l2.val : 0;
+            int digit1 = (l1 != null ) ? l1.val : 0;
+            int digit2 = (l2 != null ) ? l2.val : 0;
 
             int sum = digit1 + digit2 + carry;
             int digit = sum % 10;
             carry = sum / 10;
 
-            ListNode newNode = new ListNode(digit);
-            tail.next = newNode;
+            ListNode temp = new ListNode(digit);
+            tail.next = temp;
             tail = tail.next;
 
             l1 = (l1 != null) ? l1.next : null;
             l2 = (l2 != null) ? l2.next : null;
         }
 
-        ListNode result = temp.next;
-        temp.next = null;
+        ListNode result = ln.next;
+        ln.next = null;
         return result;
     }
-    // public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    //     int n1 = reverseList(l1, 1);
-    //     int n2 = reverseList(l2, 1);
-
-    //     int sum = n1 + n2;
-
-    //     ListNode head = null, temp = null;
-
-    //     if(sum == 0) {
-    //         ListNode newNode = new ListNode(sum);
-    //         return newNode;
-    //     }
-
-    //     while(sum > 0) {
-    //         int m = sum % 10;
-    //         ListNode newNode = new ListNode(m);
-    //         if(head == null) {
-    //             head = temp = newNode;
-    //         }
-    //         temp.next = newNode;
-    //         temp = newNode;
-    //         sum /= 10;
-    //     }
-
-    //     return head;
-    // }
-
-    // public int reverseList(ListNode head, int pos) {
-    //     if(head == null) {
-    //         return 0;
-    //     }
-
-    //     return reverseList(head.next, pos*10) + head.val * pos;
-    // }
 }
