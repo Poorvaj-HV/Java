18869ea1be231775ffb478c8bff87e466bcae0de
Poorvaj H V <poorvajvishwanath19@gmail.com>
2026-03-26 12:21:12 +0530

    Time: 5 ms (25.16%), Space: 46.7 MB (61.3%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,21 +1,19 @@
 class Solution {
     public String reverseVowels(String s) {
+        HashSet<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
         char[] ch = s.toCharArray();
         int i = 0, j = ch.length - 1;
 
         while(i < j) {
-            if((ch[i] == 'a' || ch[i] == 'e' || ch[i] == 'i' || ch[i] == 'o' || ch[i] == 'u' || ch[i] == 'A' || ch[i] == 'E' || ch[i] == 'I' || ch[i] == 'O' || ch[i] == 'U') && (ch[j] == 'a' || ch[j] == 'e' || ch[j] == 'i' || ch[j] == 'o' || ch[j] == 'u' || ch[j] == 'A' || ch[j] == 'E' || ch[j] == 'I' || ch[j] == 'O' || ch[j] == 'U')) {
+            if(!vowels.contains(ch[i])) {
+                i++;
+            } else if(!vowels.contains(ch[j])) {
+                j--;
+            } else {
                 char c = ch[i];
                 ch[i] = ch[j];
                 ch[j] = c;
                 i++;
-                j--;
-            } else if(ch[i] == 'a' || ch[i] == 'e' || ch[i] == 'i' || ch[i] == 'o' || ch[i] == 'u' || ch[i] == 'A' || ch[i] == 'E' || ch[i] == 'I' || ch[i] == 'O' || ch[i] == 'U') {
-                j--;
-            } else if(ch[j] == 'a' || ch[j] == 'e' || ch[j] == 'i' || ch[j] == 'o' || ch[j] == 'u' || ch[j] == 'A' || ch[j] == 'E' || ch[j] == 'I' || ch[j] == 'O' || ch[j] == 'U') {
-                i++;
-            } else {
-                i++; 
                 j--;
             }
         }
