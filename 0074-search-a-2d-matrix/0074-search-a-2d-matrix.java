295124bfd9d4dd688feb696db8a9170ee233d2f7
Poorvaj H V <poorvajvishwanath19@gmail.com>
2026-03-30 13:28:52 +0530

    Time: 0 ms (100%), Space: 43.9 MB (72.11%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length == 0) {
+            return false;
+        }
+        
         int row = 0;
         int col = matrix[0].length - 1;
 
