a02753b6b9511063016fe90004035dc8e738df29
Poorvaj H V <poorvajvishwanath19@gmail.com>
2026-04-01 18:53:38 +0530

    Time: 3 ms (89.06%), Space: 48.3 MB (55.94%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -6,7 +6,7 @@ class Solution {
                 return true;
             } else if(matrix[i][j] < target) {
                 i++;
-            } else if(matrix[i][j] > target) {
+            } else {
                 j--;
             }
         }
