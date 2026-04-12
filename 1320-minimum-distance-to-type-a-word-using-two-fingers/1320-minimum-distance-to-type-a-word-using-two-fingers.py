72fbd52fd6293cb75fb967ef00b6d6346990a9de
Poorvaj H V <poorvajvishwanath19@gmail.com>
2026-04-12 12:50:30 +0530

    Time: 226 ms (40.68%), Space: 53.9 MB (27.12%) - LeetHub

---
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.py
@@ -0,0 +1,36 @@
+class Solution:
+    def minimumDistance(self, word: str) -> int:
+        memo = {}
+        
+        def get_dist(char1, char2):
+            if char1 is None: return 0
+
+            c1, c2 = ord(char1) - ord('A'), ord(char2) - ord('A')
+            return abs(c1 // 6 - c2 // 6) + abs(c1 % 6 - c2 % 6)
+
+        def dp(index, f1, f2):
+
+            if index == len(word):
+                return 0
+            
+            state = (index, f1, f2)
+            if state in memo:
+                return memo[state]
+            
+            target = word[index]
+            
+
+            res1 = get_dist(f1, target) + dp(index + 1, target, f2)
+            
+
+
+
+            res2 = get_dist(f2, target) + dp(index + 1, f1, target)
+            
+            memo[state] = min(res1, res2)
+            return memo[state]
+
+
+
+
+        return dp(0, None, None);
