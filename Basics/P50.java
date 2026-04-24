c2a15c1ad75434c7777da09eeb105bea3fa223d3
Poorvaj H V <poorvajvishwanath19@gmail.com>
2026-04-24 19:49:07 +0530

    All in 1 Pattern

---
+++ Basics/P50.java
@@ -0,0 +1,34 @@
+class P50 {
+    public static void main(String[] args) {
+        int n=5;
+        int y=n-1;
+
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i-1; j++) {
+                System.out.print(n+1-j+" ");
+            }
+            for(int k=1; k<=(n-i + n-i + 1); k++) {
+                System.out.print(n+1-i+" ");
+            }
+            for(int j=i-1; j>=1; j--) {
+                System.out.print(n+1-j+" ");
+            }
+            System.out.println();
+        }
+
+        for(int i=1; i<=n-1; i++) {
+            for(int j=1; j<=n-i; j++) {
+                System.out.print(n+1-j+" ");
+            }
+            for(int k=1; k<=(i+i-1); k++) {
+                System.out.print(n+1-y+" ");
+            }
+            y--;
+            for(int j=n-i; j>=1; j--) {
+                System.out.print(n+1-j+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
