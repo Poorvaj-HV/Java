46f307478d21fb5c30d70370f815d41356a411dc
Poorvaj H V <poorvajvishwanath19@gmail.com>
2026-05-02 11:54:00 +0530

    Time: 3 ms (90.19%), Space: 44.4 MB (82.29%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<String> commonChars(String[] words) {
+        int n = words.length;
+        int[] minFreq = new int[26];
+        Arrays.fill(minFreq, Integer.MAX_VALUE);
+
+        for(String word: words) {
+            int[] charCount = new int[26];
+            for(char ch : word.toCharArray()) {
+                charCount[ch - 'a']++;
+            }
+
+            // Intersection: Keeping only the minimum count seen so far for each letter
+            for(int i=0; i<26; i++) {
+                minFreq[i] = Math.min(minFreq[i], charCount[i]);
+            }
+        }
+
+        List<String> ans = new ArrayList<>();
+        for(int i=0; i<26; i++) {
+            while(minFreq[i] > 0) {
+                ans.add(Character.toString(i + 'a'));
+                minFreq[i]--;
+            }
+        }
+
+        return ans;
+    }
+}
