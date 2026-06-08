4e41ff02a8a171ad42cf298c1ff13d492afb4896
Poorvaj H V <poorvajvishwanath19@gmail.com>
2026-06-08 18:58:11 +0530

    Time: 2 ms (57.18%), Space: 42.2 MB (41.53%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,14 +1,15 @@
 class Solution {
     int count = 0;
+
     public int totalNQueens(int n) {
-        char board[][] = new char[n][n];
-        nQueens(board, 0, n);
+        char[][] board = new char[n][n];
+        placeQueens(board, 0, n);
 
         return count;
     }
 
-    public void nQueens(char board[][], int row, int n) {
-        if(row == n) {
+    public void placeQueens(char[][] board, int row, int n) {
+        if(row >= n) {
             count++;
             return;
         }
@@ -16,7 +17,7 @@ class Solution {
         for(int i=0; i<n; i++) {
             if(isSafe(board, row, i)) {
                 board[row][i] = 'Q';
-                nQueens(board, row+1, n);
+                placeQueens(board, row+1, n);
                 board[row][i] = '\u0000';
             }
         }
@@ -24,19 +25,22 @@ class Solution {
         return;
     }
 
-    public boolean isSafe(char board[][], int row, int col) {
+    public boolean isSafe(char[][] board, int row, int col) {
+        //checking for left diagonal - both row and col changes
         for(int i=row, j=col; i>=0 && j>=0; i--, j--) {
             if(board[i][j] == 'Q') {
                 return false;
             }
         }
 
-        for(int i=row, j=col; i>=0 && j<board[row].length; i--, j++) {
+        //checking for right diagonal - both row and col changes
+        for(int i=row, j=col; i>=0 && j<board.length; i--, j++) {
             if(board[i][j] == 'Q') {
                 return false;
             }
         }
 
+        //checking for vertical - row changes, col constant
         for(int i=row; i>=0; i--) {
             if(board[i][col] == 'Q') {
                 return false;
