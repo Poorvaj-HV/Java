0e74980bd3846b74cfc0c3066bd68a29a8f3911b
Poorvaj H V <poorvajvishwanath19@gmail.com>
2026-06-11 15:50:13 +0530

    Approach - using Array

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -2,10 +2,10 @@ class Solution {
     public int calPoints(String[] operations) {
         List<Integer> ls = new ArrayList<>();
         for(String s : operations) {
-            if(s.equals("D")) {
-                ls.add(ls.get(ls.size()-1) * 2);
-            } else if(s.equals("C")) {
+            if(s.equals("C")) {
                 ls.remove(ls.size()-1);
+            } else if(s.equals("D")) {
+                ls.add(ls.get(ls.size()-1) * 2);
             } else if(s.equals("+")) {
                 ls.add(ls.get(ls.size()-1) + ls.get(ls.size()-2));
             } else {
