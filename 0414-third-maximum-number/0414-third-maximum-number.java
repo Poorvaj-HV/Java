4e80bb6c8f20e1f09a58bb869c65a899a6ae8c4a
Poorvaj H V <poorvajvishwanath19@gmail.com>
2026-06-14 19:12:35 +0530

    Time: 1 ms (98.9%), Space: 44.7 MB (71.23%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,18 +1,22 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        Set<Integer> hs = new HashSet<>();
+        long tMax = Long.MIN_VALUE;
+        long sMax = Long.MIN_VALUE;
+        long max = Long.MIN_VALUE;
+
         for(int x : nums) {
-            hs.add(x);
+            if(x > max) {
+                tMax = sMax;
+                sMax = max;
+                max = x;
+            } else if(x > sMax && x < max) {
+                tMax = sMax;
+                sMax = x;
+            } else if(x > tMax && x < sMax) {
+                tMax = x;
+            }
         }
 
-        int[] arr = new int[hs.size()];
-        int i = 0;
-        for(int x : hs) {
-            arr[i++] = x;
-        }
-
-        Arrays.sort(arr);
-
-        return arr.length >= 3 ? arr[arr.length-3] : arr[arr.length-1];
+        return (tMax == Long.MIN_VALUE) ? (int)max : (int)tMax;
     }
 }
