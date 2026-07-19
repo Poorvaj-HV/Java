b8687f6c299aa83c815d62c3d94b76a51a6df886
Poorvaj H V <poorvajvishwanath19@gmail.com>
2026-07-19 19:00:10 +0530

    Time: 1 ms (100%), Space: 44 MB (98.74%) - LeetHub

--- 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.java
+++ 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.java
@@ -1,18 +1,13 @@
 class Solution {
     public int secondsBetweenTimes(String startTime, String endTime) {
+        return timeToSeconds(endTime) - timeToSeconds(startTime);
+    }
 
-        int hours = (startTime.charAt(0) - '0') * 10 + startTime.charAt(1) - '0';
-        int minutes = (startTime.charAt(3) - '0') * 10 + startTime.charAt(4) - '0';
-        int seconds = (startTime.charAt(6) - '0') * 10 + startTime.charAt(7) - '0';
+    private int timeToSeconds(String time) {
+        int hours = (time.charAt(0) - '0') * 10 + time.charAt(1) - '0';
+        int minutes = (time.charAt(3) - '0') * 10 + time.charAt(4) - '0';
+        int seconds = (time.charAt(6) - '0') * 10 + time.charAt(7) - '0';
 
-        int sTseconds = hours * 3600 + minutes * 60 + seconds;
-
-        hours = (endTime.charAt(0) - '0') * 10 + endTime.charAt(1) - '0';
-        minutes = (endTime.charAt(3) - '0') * 10 + endTime.charAt(4) - '0';
-        seconds = (endTime.charAt(6) - '0') * 10 + endTime.charAt(7) - '0';
-
-        int eTseconds = hours * 3600 + minutes * 60 + seconds;
-
-        return eTseconds - sTseconds;
+        return hours * 3600 + minutes * 60 + seconds;
     }
 }
