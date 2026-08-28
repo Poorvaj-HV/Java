048bbbd4269d5ae4fb8087479c7bce022a4f34a8
Poorvaj H V <poorvajvishwanath19@gmail.com>
2026-08-28 10:45:52 +0530

    Time: 0 ms (100%), Space: 42.2 MB (36.02%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -3,16 +3,16 @@ class Solution {
         if(n == 1 || n == 2) {
             return n;
         }
-        
-        int[] ways = new int[n+1];
-        ways[0] = 0;
-        ways[1] = 1;
-        ways[2] = 2;
+
+        int prev1 = 1;
+        int prev2 = 2;
 
         for(int i=3; i<=n; i++) {
-            ways[i] = ways[i-1] + ways[i-2];
+            int current = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = current;
         }
 
-        return ways[n];
+        return prev2;
     }
 }
