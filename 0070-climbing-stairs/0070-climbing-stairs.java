fa29ff54be4430037f340fd04e151a997f706ef9
Poorvaj H V <poorvajvishwanath19@gmail.com>
2026-08-28 10:34:17 +0530

    Time: 0 ms (100%), Space: 42.2 MB (36.02%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,39 +1,18 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n == 44) {
-            return 1134903170;
-        } 
-        if(n == 45) {
-            return 1836311903;
+        if(n == 1 || n == 2) {
+            return n;
         }
-        if(n == 43) {
-            return 701408733;
-        }
-        if(n == 40) {
-            return 165580141;
-        }
-        if(n == 41) {
-            return 267914296;
-        }
-        if(n == 42) {
-            return 433494437;
+        
+        int[] ways = new int[n+1];
+        ways[0] = 0;
+        ways[1] = 1;
+        ways[2] = 2;
+
+        for(int i=3; i<=n; i++) {
+            ways[i] = ways[i-1] + ways[i-2];
         }
 
-        return countWays(0, n);
-    }
-
-    public int countWays(int x, int n) {
-        if(x > n) {
-            return 0;
-        }
-
-        if(x == n) {
-            return 1;
-        }
-
-        int oneStep = countWays(x+1, n);
-        int twoStep = countWays(x+2, n);
-
-        return oneStep + twoStep;
+        return ways[n];
     }
 }
